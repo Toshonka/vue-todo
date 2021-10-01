56a16077b2cfe434c83ad95b38bf621783d0c11a
Anton <ermili4ev.tony2@yandex.ru>
2021-10-01 19:37:26 +0300

    1-pract-front

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,45 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="pers-card">
+      <img alt="how" src="../assets/mandara.jpg" />
+      <h1>Карточка обо мне :)</h1>
+      <h4>Меня зовут Антон</h4>
+      <h4>Учусь в МИРЭА, группа БСБО-05-19</h4>
+      <h4>
+        Мои скиллы опишу цитатой: "Я как боксёр на ринге, ничего не вдупляю, но
+        готов ебашить"
+      </h4>
+    </div>
+    <div class="form-card">
+      <form>
+      <div class="form-field">
+        <label for="name">Ваше имя</label>
+        <input type="text" id="name" required />
+      </div>
+      <div class="form-field">
+        <label for="name">Ваше email</label>
+        <input type="text" id="name" required />
+      </div>
+      <div class="form-field">
+        <label for="name">Ваш возраст</label>
+        <input type="text" id="name" required />
+      </div>
+      <div class="submit-btn">
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
+    </div>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +57,45 @@ li {
 }
 a {
   color: #42b983;
+}
+.form-field input {
+  outline: none;
+  border: 2px solid black;
+  border-radius: 12px;
+  padding: 8px 12px;
+  margin: 10px;
+}
+.form-field label {
+  display: block;
+}
+.form-field input:focus,
+.form-field input:hover {
+  border-color: rgb(47, 179, 135);
+}
+
+.pers-card {
+  border: 2px solid black;
+  border-radius: 12px;
+  padding: 16px 16px;
+  width: 50%;
+  height: 50%;
+  margin: auto;
+  background-color: rgba(240, 174, 231, 0.623);
+  box-shadow: rgb(194, 17, 126) 10px 10px 30px;
+}
+h4 {
+  margin: 0;
+}
+.submit-btn input {
+  border: 2px solid black;
+  border-radius: 12px;
+}
+.submit-btn input:focus,
+.submit-btn input:hover {
+  border-color: rgb(47, 179, 135);
+  background-color: rgba(191, 245, 227, 0.555);
+}
+.form-card {
+  margin-top: 3%;
 }
 </style>
